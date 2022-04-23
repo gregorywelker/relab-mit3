887038c24aec52ec9048b4351c0da935832686eb
remo <gergo.welker@icloud.com>
2022-04-23 16:23:41 +0200

    task 1

--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
@@ -1,10 +1,14 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
 import org.yakindu.sct.model.sgraph.State;
 import org.yakindu.sct.model.sgraph.Statechart;
+import org.yakindu.sct.model.sgraph.Transition;
 
 import hu.bme.mit.model2gml.Model2GML;
 import hu.bme.mit.yakindu.analysis.modelmanager.ModelManager;
@@ -22,6 +26,9 @@ public class Main {
 		// Loading model
 		EObject root = manager.loadModel("model_input/example.sct");
 		
+		List<State> trapStates = new ArrayList<State>();
+		List<State> unnamedStates = new ArrayList<State>();
+		
 		// Reading model
 		Statechart s = (Statechart) root;
 		TreeIterator<EObject> iterator = s.eAllContents();
@@ -29,7 +36,31 @@ public class Main {
 			EObject content = iterator.next();
 			if(content instanceof State) {
 				State state = (State) content;
-				System.out.println(state.getName());
+		
+				if(state.getName().isEmpty()) {
+					unnamedStates.add(state);
+				}
+				if(state.getOutgoingTransitions().size() <= 0) {
+					trapStates.add(state);
+				}else {
+					for(Transition t : state.getOutgoingTransitions()) {
+						System.out.println(state.getName() + "\t->\t" + t.getTarget().getName());
+					}
+				}
+			}
+		}
+		
+		if(trapStates.size() > 0) {
+			System.out.println("Trap states:");
+			for(int i = 0; i < trapStates.size(); ++i) {
+				System.out.println("\t"+ i + ": Name: " + (trapStates.get(i).getName().isEmpty() ? "<UNNAMED STATE>" : trapStates.get(i).getName()));
+			}
+		}
+		
+		if(unnamedStates.size() > 0){
+			System.out.println("Unnamed states exist, name suggestions:");
+			for(int i = 0; i < unnamedStates.size(); ++i) {
+				System.out.println("\t" + unnamedStates.get(i).getParentRegion().getName().replace(" ", "") + "State" + i);
 			}
 		}
 		
